6bf9c477165d03bcad88db13d96fe4ed698bda90
Kshitij Sharma <kshtiij.sharma1221@gmail.com>
2025-06-11 00:18:53 +0530

    cookie send done

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,9 @@
 import User from "./../model/User.mode.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const userRegister = async (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
   try {
     const { name, email, password } = req.body;
 
@@ -25,13 +26,24 @@ export const userRegister = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user
     const user = await User.create({
       name,
       email,
       password: hashedPassword,
     });
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || "4d" }
+    );
 
+    // Set cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
     res.status(201).json({
       message: "User registered successfully",
       user: {
