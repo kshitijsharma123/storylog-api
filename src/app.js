882b8149c97caff0a170d2f52055558b9eed39a8
Kshitij Sharma <kshtiij.sharma1221@gmail.com>
2025-06-10 23:48:24 +0530

    fix user router and added a fuction to check number of req made

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import express from "express";
-
+import userRoute from "./route/user.route.js";
 const app = express();
 
-// app.use("/auth/",userRoute)
+//For checking req
+let urlHits = new Object();
+app.use((req, res, next) => {
+  const { url, ip } = req;
+  const identifier = `${url}-${ip}`;
+  urlHits[identifier] = (urlHits[identifier] || 0) + 1;
+  console.log({
+    url,
+    ip,
+    urlHitCount: urlHits[identifier],
+  });
 
+  next();
+});
+app.use("api/auth/", userRoute);
 
 export { app };
