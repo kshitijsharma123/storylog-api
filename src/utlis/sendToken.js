1d7ddd183681ad46a149087ce65bf3ca6c308201
Kshitij Sharma <kshtiij.sharma1221@gmail.com>
2025-06-11 00:30:03 +0530

    Refactoring code

---
+++ src/utlis/sendToken.js
@@ -0,0 +1,21 @@
+import { generateToken } from './jwt.js';
+
+export const sendTokenResponse = (user, res, message = 'Success') => {
+  const token = generateToken(user);
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
+    maxAge: 7 * 24 * 60 * 60 * 1000 
+  });
+
+  res.status(200).json({
+    message,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email
+    }
+  });
+};
